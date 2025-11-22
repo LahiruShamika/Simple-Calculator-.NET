eb031e0816eaffc43bbc5aa57bcecf17877c3ba3
Lahiru Shamika <159902795+LahiruShamika@users.noreply.github.com>
2025-11-22 12:14:48 +0530

    create calculator model

--- calculator/obj/Debug/net10.0/calculator.AssemblyInfo.cs
+++ calculator/obj/Debug/net10.0/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e340cf2c69eb6ad1b57b3be5ab3350f094497d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b483671874628be2544cf2d1f2bf521aef8c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
